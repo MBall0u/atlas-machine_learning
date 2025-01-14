ca953d7ea557fcc2ac72267a5fe935bc2d884f1a
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2025-01-14 14:46:24 -0600

    task 4

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""write a function that adds two arrays element-wise"""
+
+def add_arrays(arr1, arr2):
+    """returns the addition of two arrays"""
+    if len(arr1) != len(arr2):
+        return None
+
+    result_matrix = []
+    for i in range(len(arr1)):
+        result_matrix.append(arr1[i] + arr2[i])
+    return result_matrix
