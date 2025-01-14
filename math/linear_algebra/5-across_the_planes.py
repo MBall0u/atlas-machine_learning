42bc48874e7337569a3c15b0105fac3a56c8554c
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2025-01-14 14:57:12 -0600

    task 5

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""write a function that adds two 2D matrices element-wise"""
+
+
+def add_matrices2D(mat1, mat2):
+    """returns a matrix of the result of adding two matrices"""
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+
+    result_matrix = [[0 for _ in range(len(mat1[0]))] for _ in range(len(mat1))]
+    for i in range(len(mat1)):
+        for j in range(len(mat1[0])):
+            result_matrix[i][j] = mat1[i][j] + mat2[i][j]
+    return result_matrix
