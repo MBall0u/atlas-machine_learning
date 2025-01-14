3191d47e3621d5adb731a13423430a7b9dad34ef
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2025-01-14 14:59:11 -0600

    task 5

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -7,7 +7,8 @@ def add_matrices2D(mat1, mat2):
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
 
-    result_matrix = [[0 for _ in range(len(mat1[0]))] for _ in range(len(mat1))]
+    result_matrix = [[0 for _ in range(len(mat1[0]))]
+                     for _ in range(len(mat1))]
     for i in range(len(mat1)):
         for j in range(len(mat1[0])):
             result_matrix[i][j] = mat1[i][j] + mat2[i][j]
