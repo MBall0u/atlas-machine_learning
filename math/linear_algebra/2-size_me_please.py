0470f6ba29d4b1a9f4cebb5e418d4698fd430c86
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2025-01-14 14:13:26 -0600

    getting somewhere

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+"""Creating a function that returns the shape of a matrix"""
+
+
 def matrix_shape(matrix):
+    """returns the shape of a given matrix"""
     result_shape = []
     while isinstance(matrix, list):
         result_shape.append(len(matrix))
