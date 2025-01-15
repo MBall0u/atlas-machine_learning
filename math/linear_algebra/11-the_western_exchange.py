ba495320ae9bfd043f7d1bec8100a366e1c12352
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2025-01-15 13:40:38 -0600

    task 11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""a function that transposes a given matrix"""
+
+
+def np_transpose(matrix):
+    """returns a transposed version of a given matrix"""
+    transpose_matrix = np.array(matrix)
+    return np.transpose(transpose_matrix)
