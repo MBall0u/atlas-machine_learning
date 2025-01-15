984c4abfde94390a6192e8e18d13349d5c23872c
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2025-01-15 14:59:28 -0600

    task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""a function that returns th matrix multiplication"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """returns matrix multiplication"""
+    return np.matmul(mat1, mat2)
