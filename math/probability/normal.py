0da2a1e4bcdadccd229b20047d8b12e48d8a8fe2
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2025-01-22 16:34:06 -0600

    M - 4:33pm - made the normal distribution

---
+++ math/probability/normal.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""""""
+
+
+class Normal:
+    def __init__(self, data=None, mean=0., stddev=1.):
+        if stddev <= 0:
+            raise ValueError("stddev must be a positive value")
+        self.stddev = stddev
+        self.mean = mean
+
+
+        if data is None:
+            return
+        if not isinstance(data, list):
+            raise TypeError("data must be a list")
+        if len(data) < 2:
+            raise ValueError("data must contain multiple values")
+        self.mean = sum(data) / len(data)
+        variance = sum((x - self.mean) ** 2 for x in data) / len(data)
+        self.stddev = variance**variance
