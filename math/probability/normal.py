689c7dea9a10c38f90d2a04725a95e29c2c30153
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2025-01-22 16:52:30 -0600

    M - 4:33pm - made the normal distribution

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -19,4 +19,4 @@ class Normal:
             raise ValueError("data must contain multiple values")
         self.mean = sum(data) / len(data)
         variance = sum((x - self.mean) ** 2 for x in data) / len(data)
-        self.stddev = variance**variance
+        self.stddev = variance**0.5
