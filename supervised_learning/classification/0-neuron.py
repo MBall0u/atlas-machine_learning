cf6ea5c34019f2d9c1015a197cf9f9e8d6de14f5
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2025-02-02 14:54:54 -0600

    M - 2:53pm - fixed task one

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -6,11 +6,11 @@ import numpy as np
 class Neuron:
     """defining a single neuron"""
     def __init__(self, nx):
-        if not int(nx):
+        if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         self.nx = nx
-        self.W = np.random.randn(nx)
+        self.W = np.random.normal(size=nx)
         self.b = 0
         self.A = 0
