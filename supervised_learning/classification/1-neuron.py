a861902f3c6e6e6d0991c6bf164087f06b512e38
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2025-02-02 15:28:30 -0600

    made the attributes private and added getter methods

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""defining a single neuron performing binary classification"""
+import numpy as np
+
+
+class Neuron:
+    """defining a single neuron"""
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """getter method"""
+        return self.__W
+
+    @property
+    def b(self):
+        """getter method"""
+        return self.__b
+
+    @property
+    def A(self):
+        """getter method"""
+        return self.__A
