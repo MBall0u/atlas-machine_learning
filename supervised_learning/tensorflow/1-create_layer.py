360201917a158fb8354292d77f8c542a7fb17fe0
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2025-02-20 13:57:38 -0600

    layers

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -5,10 +5,11 @@ import tensorflow.compat.v1 as tf
 
 def create_layer(prev, n, activation):
     """creates a layer"""
+    init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
         name="layer",
-        kernel_initializer=tf.keras.initializers.VarianceScaling(mode='fan_avg')
+        kernel_initializer=init
     )
     return layer(prev)
