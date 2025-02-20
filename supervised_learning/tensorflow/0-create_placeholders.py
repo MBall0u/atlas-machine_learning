c7ec41a8d25e22b1651810064b8c838ac67547a5
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2025-02-20 11:11:24 -0600

    placeholders

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -2,7 +2,9 @@
 """ Placeholders """
 import tensorflow.compat.v1 as tf
 
+
 def create_placeholders(nx, classes):
+    """ creating placeholders """
     x = tf.placeholder(tf.float32, shape=(None, nx), name='x')
     y = tf.placeholder(tf.float32, shape=(None, classes), name='y')
     return x, y
